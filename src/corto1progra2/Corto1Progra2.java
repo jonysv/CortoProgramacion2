a1ff1eda09a4a06f6c2578dd2a49971c61aeb1ba
elmer <elmer@elmer>
2019-03-26 21:46:30 -0600

    Ventana (guardar,modificar,eliminar)

---
+++ src/corto1progra2/Corto1Progra2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corto1progra2;
+
+/**
+ *
+ * @author elmer
+ */
+public class Corto1Progra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Ventana vista = new Ventana();
+        vista.setVisible(true);
+    }
+    
+}
